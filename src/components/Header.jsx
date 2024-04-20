b4046fd7868c16c3fe7585d078e8bb56189a70e4
Mohamedkhaled68 <mohamed.khaled8ooo8@gmail.com>
2024-04-20 11:58:38 +0200

    GG

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,79 +8,70 @@ import { useSelector } from "react-redux";
 import { useAuth } from "../contexts/AuthContext";
 
 const Header = () => {
-    const cart = useSelector((state) => state.cart.productsNumber);
-    const { currentUser } = useAuth();
+  const cart = useSelector((state) => state.cart.productsNumber);
+  const { currentUser, name } = useAuth();
 
-    const name = JSON.parse(localStorage.getItem("users"))?.map((user) => {
-        if (currentUser?.uid == user.uid) {
-            return user.username;
-        }
-    });
-
-    return (
-        <>
-            <div className="header px-4 py-1 bg-slate-950 flex justify-between items-center gap-10 text-white">
-                {/* left */}
-                <div className="flex items-center gap-4">
-                    <Link to={"/"}>
-                        <img
-                            className="w-[100px] h-[35px] mt-4 object-fit"
-                            src="../images/amazon.png"
-                            alt="header-logo"
-                        />
-                    </Link>
-                    <div className="flex">
-                        <CiLocationOn className="self-end" size={25} />
-                        <div>
-                            <div className="text-xs text-slate-400">
-                                deliver to
-                            </div>
-                            <div className="text-sm font-bold">Egypt</div>
-                        </div>
-                    </div>
-                </div>
-                {/* Middle */}
-                <Search />
-                {/* Right */}
-                <div className="flex items-center gap-5">
-                    <div>
-                        <div className="text-xs">
-                            Hello,{" "}
-                            {currentUser ? (
-                                <Link to={"/profile"}>{name}</Link>
-                            ) : (
-                                <Link to={"/login"}>sign in</Link>
-                            )}
-                        </div>
-                        <div className="text-sm font-bold cursor-pointer">
-                            Account & Lists{" "}
-                            <IoMdArrowDropdown className="inline" />
-                        </div>
-                    </div>
-                    <div>
-                        <div className="text-xs ">Returns</div>
-                        <div className="text-sm font-bold">& Orders</div>
-                    </div>
-                    <Link to={"/checkout"} className="flex gap-2 items-end">
-                        <div className="relative">
-                            <span className="absolute top-[-15px] right-2 text-amazonYellow font-semibold">
-                                {cart}
-                            </span>
-                            <FiShoppingCart size={30} />
-                        </div>
-                        <div className="text-base font-bold">basket</div>
-                    </Link>
-                </div>
+  return (
+    <>
+      <div className="header px-4 py-1 bg-slate-950 flex justify-between items-center gap-10 text-white">
+        {/* left */}
+        <div className="flex items-center gap-4">
+          <Link to={"/"}>
+            <img
+              className="w-[100px] h-[35px] mt-4 object-fit"
+              src="../images/amazon.png"
+              alt="header-logo"
+            />
+          </Link>
+          <div className="flex">
+            <CiLocationOn className="self-end" size={25} />
+            <div>
+              <div className="text-xs text-slate-400">deliver to</div>
+              <div className="text-sm font-bold">Egypt</div>
             </div>
-            <div className="flex gap-3 items-center px-4 py-2 text-white text-sm bg-amazonLight_blue">
-                <div>Today's Deals</div>
-                <div>Customer Service</div>
-                <div>Registry</div>
-                <div>Sell</div>
-                <div>Gift Cards</div>
+          </div>
+        </div>
+        {/* Middle */}
+        <Search />
+        {/* Right */}
+        <div className="flex items-center gap-5">
+          <div>
+            <div className="text-xs">
+              Hello,{" "}
+              {currentUser ? (
+                <Link to={"/profile"}>{name}</Link>
+              ) : (
+                <Link to={"/login"}>sign in</Link>
+              )}
             </div>
-        </>
-    );
+            <div className="text-sm font-bold cursor-pointer">
+              Account & Lists <IoMdArrowDropdown className="inline" />
+            </div>
+          </div>
+          <div>
+            <div className="text-xs ">Returns</div>
+            <div className="text-sm font-bold">& Orders</div>
+          </div>
+          <Link to={"/checkout"} className="flex gap-2 items-end">
+            <div className="relative">
+              <span className="absolute top-[-15px] right-2 text-amazonYellow font-semibold">
+                {cart}
+              </span>
+              <FiShoppingCart size={30} />
+            </div>
+            <div className="text-base font-bold">basket</div>
+          </Link>
+        </div>
+      </div>
+      <div className="flex gap-3 items-center px-4 py-2 text-white text-sm bg-amazonLight_blue">
+        <div>Today's Deals</div>
+        <div>Customer Service</div>
+        <div>Registry</div>
+        <div>Sell</div>
+        <div>Gift Cards</div>
+      </div>
+    </>
+  );
 };
 
 export default Header;
--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,49 +1,59 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import {
-    createUserWithEmailAndPassword,
-    onAuthStateChanged,
-    signInWithEmailAndPassword,
-    signOut,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
 } from "firebase/auth";
 import { auth } from "../firebase/firebase";
 
 const AuthContext = createContext({});
 
 const AuthProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
-    const [loading, setLoading] = useState(true);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const login = (email, password) => {
-        return signInWithEmailAndPassword(auth, email, password);
+  const login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const signup = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const logout = () => {
+    return signOut(auth);
+  };
+  const capitalize = (str) => {
+    const lower = str.toLowerCase();
+    return str.charAt(0).toUpperCase() + lower.slice(1);
+  };
+
+  const name = JSON.parse(localStorage.getItem("users"))?.map((user) => {
+    if (currentUser?.uid == user.uid) {
+      return capitalize(user.username);
+    }
+  });
+
+  useEffect(() => {
+    const listener = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return () => {
+      listener();
     };
-
-    const signup = (email, password) => {
-        return createUserWithEmailAndPassword(auth, email, password);
-    };
-    const logout = () => {
-        return signOut(auth);
-    };
-
-    useEffect(() => {
-        const listener = onAuthStateChanged(auth, (user) => {
-            setCurrentUser(user);
-            setLoading(false);
-        });
-
-        return () => {
-            listener();
-        };
-    }, []);
-    const values = { login, signup, currentUser, logout };
-    return (
-        <AuthContext.Provider value={values}>
-            {!loading && children}
-        </AuthContext.Provider>
-    );
+  }, []);
+  const values = { login, signup, currentUser, logout, name };
+  return (
+    <AuthContext.Provider value={values}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
 
 export const useAuth = () => {
-    return useContext(AuthContext);
+  return useContext(AuthContext);
 };
